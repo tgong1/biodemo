a1bbb79d19f2ba73f9496fa8976874076d9572e2
Ubuntu <ubuntu@None.novalocal>
2018-11-30 03:53:16 +0000

    Gather Code Coverage Metrics

--- .travis/unit-test.sh
+++ .travis/unit-test.sh
@@ -4,7 +4,7 @@ set -e
 errors=0
 
 # Run unit tests
-python biodemo/biodemo_test.py || {
+coverage run -m unittest discover biodemo || {
     echo "'python python/biodemo/biodemo_test.py' failed"
     let errors+=1
 }
